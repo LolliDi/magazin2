8c969cc9e1c4f054d3765e6ea670ab41c81c5299
Lolli Di <290romchik@gmail.com>
2021-10-25 21:28:12 +0300

    Добавление в базу, в корзину, оформелние заказа, сброс

---
+++ magazin/app/src/main/java/com/example/magazin/DBHelper.java
@@ -0,0 +1,35 @@
+package com.example.magazin;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper  extends SQLiteOpenHelper{
+
+    public static final int db_ver = 1;
+    public static final String db_name = "contactDb";
+    public static final String tb_contacts = "contacts";
+
+    public static final String KEY_ID = "_id";
+    public static final String nazv = "nazv";
+    public static final String sprice = "cena";
+
+    public DBHelper(Context context) {
+        super(context, db_name, null, db_ver);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + tb_contacts + "("+ KEY_ID
+                + " integer primary key," + nazv + " text," + sprice + " REAL" + ")");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + tb_contacts);
+
+        onCreate(db);
+
+    }
+}
